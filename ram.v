445806cf6e15bbf9f3f8ac755b91bcadc4f4ab29
Alphy <alphiya01@mail.ru>
2018-10-21 11:15:56 +0300

    Ram_second

--- ram.v
+++ ram.v
@@ -1,10 +1,17 @@
 module (
 	input [0:31] address,
 	input [0:31] data,
-	input write
-	input response,
+	input write,
+	input clk,
+	output response,
 	output [0:31] out
 );
 
 parameter size = 4096;
-reg [0:31] ram []
+reg [0:size - 1] ram [0:31];
+
+reg [0:31] prev_adress,
+reg [0:31] prev_data,
+reg write,
+reg prev_response,
+
---
+++ ram.v.bak
@@ -0,0 +1,10 @@
+module (
+	input [0:31] address,
+	input [0:31] data,
+	input write
+	input response,
+	output [0:31] out
+);
+
+parameter size = 4096;
+reg [0:31] ram []
